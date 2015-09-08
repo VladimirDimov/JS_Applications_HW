f91e52c542832f5b625e1fd630f1f44caff3d50d
VladimirDimov <vdimov1124@gmail.com>
2015-09-08 15:12:35 +0300

    Practice exam 2014

--- Practice/crowd share - exam 2014/crowd-share-rest-api - vladko/public/js/app.js
+++ Practice/crowd share - exam 2014/crowd-share-rest-api - vladko/public/js/app.js
@@ -1,29 +1,28 @@
-
-var sammyApp = new Sammy('#posts', function () {
-	this.get('#/', function (context) {
+var sammyApp = new Sammy('#posts', function() {
+	this.get('#/', function(context) {
 		context.redirect('#/home');
 	});
 
-	this.get('#/home', function (context) {
+	this.get('#/home', function(context) {
 		var currentUser = JSON.parse(localStorage.getItem('CURRENT_USER'));
 
 		if (currentUser) {
 			context.redirect('#/home/user');
 		} else {
 			templates.get('home')
-				.then(function (template) {
+				.then(function(template) {
 					loadContent(template());
 				});
 		}
 	});
 
-	this.get('#/register', function (context) {
+	this.get('#/register', function(context) {
 		templates.get('register')
-			.then(function (template) {
+			.then(function(template) {
 				loadContent(template());
 			})
-			.then(function () {
-				$('#btn-register').click(function () {
+			.then(function() {
+				$('#btn-register').click(function() {
 					var user = {
 						username: $('#tb-username').val(),
 						authCode: CryptoJS.SHA1($('#tb-username').val()).toString()
@@ -33,20 +32,20 @@ var sammyApp = new Sammy('#posts', function () {
 			});
 	});
 
-	this.get('#/login', function (context) {
+	this.get('#/login', function(context) {
 		templates.get('login')
-			.then(function (template) {
+			.then(function(template) {
 				loadContent(template());
 			})
-			.then(function () {
-				$('#btn-login').click(function () {
+			.then(function() {
+				$('#btn-login').click(function() {
 					var user = {
 						username: $('#tb-username').val(),
 						authCode: CryptoJS.SHA1($('#tb-username').val()).toString()
 					}
 
 					services.user.login(user)
-						.then(function (res) {
+						.then(function(res) {
 							services.user.setCurrentUser(res);
 							context.redirect('#/home/user');
 						})
@@ -55,26 +54,85 @@ var sammyApp = new Sammy('#posts', function () {
 
 	});
 
-	this.get('#/logout', function (context) {
+	this.get('#/logout', function(context) {
 		localStorage.removeItem('CURRENT_USER');
 		context.redirect('#/home');
 	});
 
-	this.get('#/post/create', function (context) {
+	this.get('#/post/create', function(context) {
 		templates.get('create-post')
-			.then(function (template) {
+			.then(function(template) {
 				loadContent(template());
 			})
-			.then(function () {
+			.then(function() {
 				$('#btn-create').click(function() {
-					
+					var newPost = {
+						title: $('#tb-title').val(),
+						body: $('#tb-body').val()
+					}
+					services.posts.add(newPost)
+						.then(function(res) {
+							console.log(res);
+							// TODO: post created
+						})
 				});
 			})
 	});
 
-	this.get('#/home/user', function (context) {
+	this.get('#/post/get', function(context) {
+		var filteredPosts;
+
+		getPosts(null, null, 2, 0)
+			.then(function(res) {
+				filteredPosts = res;
+				return templates.get('posts')
+			})
+			.then(function(template) {
+				loadContent(template(filteredPosts));
+
+				$('#btn-filter-posts').click(function() {
+					var byUser = $('#tb-filter-user').val() || '#';
+					var byPattern = $('#tb-filter-pattern').val() || '#';
+					var pageSize = $('#page-size').val();
+					var pageNumber = $('#page-number').val();
+
+					context.redirect('#/post/get/' +
+						byUser + '/' + byPattern + '/' + pageSize + '/' + pageNumber);
+				});
+			})
+	});
+
+	this.get('#/post/get/:byUser/:byPattern/:pageSize/:pageNumber', function(context) {
+		var filteredPosts;
+		var byUser = context.params.byUser;
+		var byPattern = context.params.byPattern;
+		var pageSize = context.params.pageSize;
+		var pageNumber = context.params.pageNumber;
+
+
+		getPosts(byUser, byPattern, pageSize, pageNumber)
+			.then(function(res) {
+				filteredPosts = res;
+				return templates.get('posts')
+			})
+			.then(function(template) {
+				loadContent(template(filteredPosts));
+
+				$('#btn-filter-posts').click(function() {
+					var byUser = $('#tb-filter-user').val() || '#';
+					var byPattern = $('#tb-filter-pattern').val() || '#';
+					var pageSize = $('#page-size').val();
+					var pageNumber = $('#page-number').val();
+
+					context.redirect('#/post/get/' +
+						byUser + '/' + byPattern + '/' + pageSize + '/' + pageNumber);
+				});
+			})
+	});
+
+	this.get('#/home/user', function(context) {
 		templates.get('user-homepage')
-			.then(function (template) {
+			.then(function(template) {
 				loadContent(template());
 			});
 	});
@@ -83,10 +141,36 @@ var sammyApp = new Sammy('#posts', function () {
 sammyApp.run('#/');
 
 function loadContent(content) {
-	var promise = new Promise(function (resolve, reject) {
+	var promise = new Promise(function(resolve, reject) {
 		$('#posts')
 			.empty()
 			.append(content);
+	});
+
+	return promise;
+}
+
+function getPosts(byUser, byPattern, pageSize, pageNumber) {
+	var promise = new Promise(function(resolve, reject) {
+		var posts;
+		if (byUser === '#') {
+			byUser = null
+		};
+		if (byPattern === '#') {
+			byPattern = null
+		};
+
+		pageSize = Number(pageSize);
+		pageNumber = Number(pageNumber) - 1;
+
+		services.posts.get(byUser, byPattern)
+			.then(function(res) {
+				posts = res;
+				var leftIndex = pageSize * pageNumber;
+				var rightIndex = leftIndex + pageSize;
+				posts = posts.slice(leftIndex, rightIndex);
+				resolve(posts);
+			});
 	});
 
 	return promise;
--- Practice/crowd share - exam 2014/crowd-share-rest-api - vladko/public/js/services.js
+++ Practice/crowd share - exam 2014/crowd-share-rest-api - vladko/public/js/services.js
@@ -1,16 +1,18 @@
-var services = (function () {
+var services = (function() {
 	var user = {
-		register: function (user) {
-			var promise = new Promise(function (resolve, reject) {
+		register: function(user) {
+			var promise = new Promise(function(resolve, reject) {
 				var url = 'user';
 				$.ajax(url, {
 					type: 'POST',
 					contentType: 'application/json',
 					data: JSON.stringify(user),
-					success: function (res) {
+					success: function(res) {
+						toastr['success']('User registered');
 						resolve(res);
 					},
-					error: function (err) {
+					error: function(err) {
+						toastr['error']('User not registered. ' + err.textMessage);
 						reject(err);
 					}
 				});
@@ -19,17 +21,19 @@ var services = (function () {
 			return promise;
 		},
 
-		login: function (user) {
-			var promise = new Promise(function (resolve, reject) {
+		login: function(user) {
+			var promise = new Promise(function(resolve, reject) {
 				var url = 'auth';
 				$.ajax(url, {
 					type: 'POST',
 					contentType: 'application/json',
 					data: JSON.stringify(user),
-					success: function (res) {
+					success: function(res) {
+						toastr['success']('User logged in.');
 						resolve(res);
 					},
-					error: function (err) {
+					error: function(err) {
+						toastr['error']('User could not login. ' + err.textMessage);
 						reject(err);
 					}
 				});
@@ -40,10 +44,74 @@ var services = (function () {
 
 		setCurrentUser(user) {
 			localStorage.setItem('CURRENT_USER', JSON.stringify(user));
+		},
+
+		getCurentUser() {
+			return JSON.parse(localStorage.getItem('CURRENT_USER'));
 		}
 	};
 
-	return {
-		user: user
+	var posts = {
+		add: function(post) {
+			var currentUser = services.user.getCurentUser();
+
+			var promise = new Promise(function(resolve, reject) {
+				$.ajax('post', {
+					method: 'POST',
+					contentType: 'application/json',
+					data: JSON.stringify(post),
+					headers: {
+						'X-SessionKey': currentUser.sessionKey
+					},
+					success: function(res) {
+						toastr['success']('Post created!');
+						resolve(res);
+					},
+					error: function(err) {
+						toastr['error']('Could not create new post. ' + err.textMessage);
+						reject(err);
+					}
+				});
+			});
+
+			return promise;
+		},
+
+		get: function(byUser, byPattern) {
+			var url = 'post';
+			var filterParameters = [];
+			if (byUser) {
+				filterParameters.push('user=' + byUser);
+			} 
+
+			if (byPattern) {
+				filterParameters.push('pattern=' + byPattern);
+			}
+
+			if (filterParameters.length > 0) {
+				url += '?' + filterParameters.join('&');
+			};
+
+			var promise = new Promise(function(resolve, reject) {
+				$.ajax(url, {
+					method: 'GET',
+					contentType: 'json',
+					success: function(res) {
+						resolve(res);
+					},
+					error: function(err) {
+						toastr['error']('Error loading posts');
+						reject(err);
+					}
+				})
+			});
+
+			return promise;
+		}
 	}
-} ());
+
+	return {
+		user: user,
+		posts: posts
+	}
+}());
